b368b425124df29617681c9618c20680a11fa2b5
Markus Kasten <github@markuskasten.eu>
2021-12-17 11:49:25 +0100

    feat: first draft of gym implementation

---
+++ tinycarlo/env.py
@@ -0,0 +1,47 @@
+import math
+
+import numpy as np
+import gym
+
+class CarSimEnv(gym.Env):
+    def __init__(self):
+        # action space: (velocity, steering angle)
+        self.action_space = gym.spaces.Box(-1, 1, shape=(2,))
+        self.observation_space = gym.spaces.Box(low=0, high=255, shape=(100, 200), dtype=np.uint8)
+        
+        self.reset()
+
+    def simulate_step(self, fwd_vel, steering_angle):
+        dt = 1
+        wheelbase = 0.2
+        
+        ang_vel = (math.tan(steering_angle) / wheelbase) * fwd_vel
+
+        vx = fwd_vel * math.cos(self.car_rotation)
+        vy = fwd_vel * math.sin(self.car_rotation)
+
+        dx = (vx * math.cos(self.car_rotation) - vy * math.sin(self.car_rotation)) * dt
+        dy = (vx * math.sin(self.car_rotation) + vy * math.cos(self.car_rotation)) * dt
+        dyaw = ang_vel * dt
+
+        self.car_position[0] += dx
+        self.car_position[1] += dy
+        self.car_rotation += dyaw
+        
+    def step(self, action):
+        observation = None
+        reward = 0
+        done = False
+        info = None
+        
+        self.simulate_step(1.0, 0.5)
+        
+        return None, reward, done, info
+        
+    def reset(self):
+        self.car_position = np.zeros((2,))
+        self.car_velocity = 0.0
+        self.car_rotation = 0.0
+    
+    def render(self, mode='human', close=False):
+        pass
