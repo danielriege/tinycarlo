4020ea622766925f6138f668d69121923aa7dd6e
Markus Kasten <github@markuskasten.eu>
2021-12-17 12:38:50 +0100

    chore: add Pipfile, minor cleanup

---
+++ tinycarlo/__init__.py
@@ -0,0 +1,3 @@
+from gym.envs.registration import register
+
+register(id="tinycarlo-v0", entry_point="tinycarlo.env:TinyCarloEnv")
--- tinycarlo/env.py
+++ tinycarlo/env.py
@@ -3,19 +3,24 @@ import math
 import numpy as np
 import gym
 
-class CarSimEnv(gym.Env):
+
+class TinyCarloEnv(gym.Env):
     def __init__(self):
         # action space: (velocity, steering angle)
         self.action_space = gym.spaces.Box(-1, 1, shape=(2,))
-        self.observation_space = gym.spaces.Box(low=0, high=255, shape=(100, 200), dtype=np.uint8)
-        
+        self.observation_space = gym.spaces.Box(
+            low=0, high=255, shape=(100, 200), dtype=np.uint8
+        )
+
+        self.wheelbase = 0.15
+        self.mass = 0.2
+
         self.reset()
 
     def simulate_step(self, fwd_vel, steering_angle):
         dt = 1
-        wheelbase = 0.2
-        
-        ang_vel = (math.tan(steering_angle) / wheelbase) * fwd_vel
+
+        ang_vel = (math.tan(steering_angle) / self.wheelbase) * fwd_vel
 
         vx = fwd_vel * math.cos(self.car_rotation)
         vy = fwd_vel * math.sin(self.car_rotation)
@@ -27,21 +32,20 @@ class CarSimEnv(gym.Env):
         self.car_position[0] += dx
         self.car_position[1] += dy
         self.car_rotation += dyaw
-        
+
     def step(self, action):
         observation = None
         reward = 0
         done = False
         info = None
-        
-        self.simulate_step(1.0, 0.5)
-        
-        return None, reward, done, info
-        
+
+        self.simulate_step(*action)
+
+        return observation, reward, done, info
+
     def reset(self):
         self.car_position = np.zeros((2,))
-        self.car_velocity = 0.0
         self.car_rotation = 0.0
-    
-    def render(self, mode='human', close=False):
+
+    def render(self, mode="human", close=False):
         pass
