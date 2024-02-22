61ca87607a0d9808425ef2a2ee09a440835dc1be
Daniel Riege <daniel.riege99@gmail.com>
2024-02-22 12:52:22 +0100

    updating setup

--- setup.py
+++ setup.py
@@ -5,18 +5,28 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='tinycarlo',  
-     version='1.1',
-     author="Daniel Riege, Markus Kasten",
-     description="2D car simulation",
+     version='2.0.0',
+     author="Daniel Riege",
+     description="Think of Carla but much lighter and simpler or the Car Racing gym but more serious",
+     license="MIT",
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/danielriege/tinycarlo",
-     packages=setuptools.find_packages(),
+     packages=["tinycarlo", "tinycarlo.wrapper"],
      install_requires=[
         'gymnasium>=0.26.0',
         'numpy>=1.22.0',
         'opencv-python>=4.5.5.62',
         'setuptools>=60.3.1',
         'pyyaml>=6.0'
-     ]
+     ],
+     python_requires='>=3.8',
+     extras_require={
+        'testing': ['pytest'],    
+    },
+     entry_points={
+        'console_scripts': [
+            'tinycarlo.mapbuilder=mapbuilder.mapbuilder:main',
+        ],
+    },
  )
